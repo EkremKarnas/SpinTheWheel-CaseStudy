388b333213cafffb8a9c787f8008db946a8c3260
Ekrem Karnas <ekremkarnas01@gmail.com>
2022-12-12 00:51:01 +0300

    Some improvement

--- SpinTheWheel/Assets/SpinTheWheel.cs
+++ SpinTheWheel/Assets/SpinTheWheel.cs
@@ -10,30 +10,44 @@ public class SpinTheWheel : MonoBehaviour
     private bool isSpinning;
     [SerializeField] int spinningTime;
     [SerializeField] float spinningAngle;
+    [SerializeField] ObjectTransporter objectTransporter;
     [SerializeField] DropDown dropdown;
     private float extraAngle;
+    private int itemNumber;
+    private float randomProbablity;
     void Start()
     {
         ProbablityCalculator();
-        spinningAngle = spinningAngle + (360 * Random.Range(3,5));
+        spinningAngle = spinningAngle + (360 * Random.Range(3, 5));
         spinningAngle = spinningAngle + extraAngle;
 
-        Debug.Log(spinningAngle);
+        // Debug.Log(spinningAngle);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isSpinning)
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            StartCoroutine(SpinTheWheelFunc(spinningTime, spinningAngle));
+            var name = dropdown.transformList[3].GetComponentInChildren<Rigidbody>().gameObject;
+            Instantiate(name, Vector3.up, Quaternion.identity);
         }
     }
 
     public void ButtonFunc()
     {
-        if(!isSpinning)
+        if (!isSpinning)
         {
             StartCoroutine(SpinTheWheelFunc(spinningTime, spinningAngle));
+
+            // var gameObject = dropdown.transformList[itemNumber].GetComponentInChildren<Rigidbody>().gameObject;
+            // objectTransporter.MoveObject(gameObject);
+            StartCoroutine(WaitAndMoveFunc());
+
+            spinningAngle = 0;
+
+            ProbablityCalculator();
+            spinningAngle = spinningAngle + (360 * Random.Range(3, 5));
+            spinningAngle = spinningAngle + extraAngle;
         }
     }
 
@@ -54,9 +68,21 @@ public class SpinTheWheel : MonoBehaviour
         isSpinning = false;
     }
 
+    IEnumerator WaitAndMoveFunc()
+    {
+        
+
+        yield return new WaitForSeconds(spinningTime);
+        var gameObject = dropdown.transformList[itemNumber].GetComponentInChildren<Rigidbody>().gameObject;
+        objectTransporter.MoveObject(gameObject);
+        Debug.Log(randomProbablity);
+        Debug.Log(itemNumber);
+        // itemNumber = -1;
+    }
+
     void ProbablityCalculator()
     {
-        var randomProbablity = Random.Range(1f, 100f);
+        randomProbablity = Random.Range(1f, 100f);
 
         var probablityTreshold1 = dropdown.slot1DropProbablity;
         var probablityTreshold2 = dropdown.slot1DropProbablity + dropdown.slot2DropProbablity;
@@ -71,41 +97,49 @@ public class SpinTheWheel : MonoBehaviour
         if (randomProbablity < probablityTreshold1)
         {
             extraAngle = 0;
+            itemNumber = 0;
         }
 
         else if (randomProbablity >= probablityTreshold1 && randomProbablity < probablityTreshold2)
         {
             extraAngle = 45f;
+            itemNumber = 1;
         }
 
         else if (randomProbablity >= probablityTreshold2 && randomProbablity < probablityTreshold3)
         {
             extraAngle = 90;
+            itemNumber = 2;
         }
 
         else if (randomProbablity >= probablityTreshold3 && randomProbablity < probablityTreshold4)
         {
             extraAngle = 135;
+            itemNumber = 3;
         }
 
         else if (randomProbablity >= probablityTreshold4 && randomProbablity < probablityTreshold5)
         {
             extraAngle = 180;
+            itemNumber = 4;
         }
 
         else if (randomProbablity >= probablityTreshold5 && randomProbablity < probablityTreshold6)
         {
             extraAngle = 225;
+            itemNumber = 5;
         }
 
         else if (randomProbablity >= probablityTreshold6 && randomProbablity < probablityTreshold7)
         {
             extraAngle = 270;
+            itemNumber = 6;
         }
 
         else if (randomProbablity >= probablityTreshold7)
         {
             extraAngle = 315;
+            itemNumber = 7;
         }
 
     }
